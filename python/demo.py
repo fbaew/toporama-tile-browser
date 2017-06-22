a321d7e1c273aeb04e5f2fc431cfa85ef20a72c2
Gregg Lewis <code@gregglewis.net>
2017-06-22 01:32:13 -0600

    grab a bigass image

--- python/demo.py
+++ python/demo.py
@@ -1,17 +1,81 @@
 from owslib.wms import WebMapService
-wms = WebMapService('http://wms.ess-ws.nrcan.gc.ca/wms/toporama_en')
-wms.identification.type
-wms.identification.version
-wms.identification.title
-list(wms.contents)
-list(wms.contents)
-[op.name for op in wms.operations]
-wms.getOperationByName('GetMap')
-wms.getOperationByName('GetMap').methods
-wms.getOperationByName('GetMap').formatOptions
-img = wms.getmap( layers=['WMS-Toporama'], srs='EPSG:3978',bbox=(-1345995, 412199, -1288466, 439592), size=(1024,768), format='image/png', transparent=False)
 
-image = open('img1.png', 'wb')
-image.write(img.read())
-image.close()
+class TileGrabber:
 
+    def __init__(self):
+        self.wms = WebMapService('http://wms.ess-ws.nrcan.gc.ca/wms/toporama_en')
+    
+    
+    def selfTest(self):
+
+        print(self.wms.identification.type)
+        print(self.wms.identification.version)
+        print(self.wms.identification.title)
+        print(list(self.wms.contents))
+    
+    
+        print("Operations:")
+        print([op.name for op in self.wms.operations])
+    
+        print("methods:")
+        print(Wms.getOperationByName('GetMap').methods)
+    
+        print("formatOperations")
+        print(Wms.getOperationByName('GetMap').formatOptions)
+
+
+    def getImageSize(self, nad_size, target_size):
+
+        (nad_x, nad_y) = nad_size
+        (target_x, target_y) = target_size
+
+        target_y = target_x / nad_y * nad_x
+
+        return (target_y, target_x)
+            
+        
+    
+    def getTile(self, bbox): 
+
+        (x1, y1, x2, y2) = bbox
+
+
+        nad_width = x1-x2
+        nad_height = y1-y2
+        target_size = (2850, 1000)
+
+        print("Attempting to get tile of size {}".format(target_size))
+        image_size = self.getImageSize((nad_width, nad_height), target_size)
+        print("Actual image size: {}".format(image_size))
+
+        img = self.wms.getmap(
+            layers=['WMS-Toporama'],
+            srs='EPSG:3978',
+            bbox=(x1, y1, x2, y2),
+            size=image_size,
+            format='image/png',
+            transparent=False
+        )
+        return img
+    
+    def saveImage(self, imagedata, filename):
+
+        image = open(filename, 'wb')
+        image.write(imagedata.read())
+        image.close()
+
+
+def main():
+
+    x1 = -1345995
+    y1 = 412199
+    x2 = -1288466
+    y2 = 439592
+
+    tiler = TileGrabber()
+
+    img = tiler.getTile((x1,y1,x2,y2))
+    tiler.saveImage(img,'tile1.png')
+
+if __name__ == '__main__':
+    main()
